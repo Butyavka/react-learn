46b3769e7ffcbc6693bc2181e1ace2efa4aa6654
Butyavka <oxana199509@gmail.com>
2021-09-13 18:50:25 +0300

    commit 1.3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {HashRouter, Route} from 'react-router-dom'
 import About from './routes/About'
 import Home from './routes/Home'
