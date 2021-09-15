e75db4c0ca5d6b9b20482298400311138db5f9d6
Butyavka <oxana199509@gmail.com>
2021-09-15 15:52:13 +0300

    1.10

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 const Navigation = () => {
     return (
         <nav className="navigation__list">
-            <Link to="/" className="navigation__link">Главная</Link>
+            <Link to="/react-learn" className="navigation__link">Главная</Link>
             <Link to="/about" className="navigation__link">О проекте</Link>
         </nav>
     )
