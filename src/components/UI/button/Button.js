f370fe0c6ec69928afe7350ef06b116945327b1b
Butyavka <oxana199509@gmail.com>
2021-09-15 15:45:26 +0300

    1.6

--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../../styles/button.css"
+import "./button.css"
 
 const Button = ({buttonClass, text, onClick}) => {
     return (
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ import Home from "../containers/Home";
 import Login from "../components/Login";
 
 export const privateRoutes = [
-    {path: '/', component: Home, exact: true},
+    {path: '/react-learn', component: Home, exact: true},
     {path: '/about', component: About, exact: true},
     {path: '/movie/:id', component: Detail, exact: true},
     {path: '/error404', component: Error404, exact: true},
